104ae4dd9f66e6037962777f6c38ac089afaad29
jongseung415 <jongseung415@gmail.com>
2024-06-20 16:55:44 +0900

    0620 studied

---
+++ practicePython/marks1.py
@@ -0,0 +1,24 @@
+marks = [90,25,67,45,80]
+#counter = 0
+# for mark in marks:
+#     counter += 1
+#     if mark >= 60:
+#         print("%d번 합격" % counter)
+#     else:
+#         continue
+
+# for counter in range(len(marks)):
+#     if marks[counter] < 60:
+#         continue
+#     print("%d번 햅격!" % (counter + 1))
+
+# result  = 0
+# for totalAdd in range(101):
+#     result = result + totalAdd
+#
+# print(result)
+
+for mainNumber in range(2,10):
+    for i in range(1,10):
+        print(mainNumber * i, end=" ")
+    print(' ')
